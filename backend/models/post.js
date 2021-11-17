d48e895dccb4f89bfca0c710b4465646cfaa339b
juancamilorv <juancrv28@gmail.com>
2021-11-17 16:42:29 -0500

    CRUD update

---
+++ backend/models/post.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+    
+    id: { type: Number},
+    user: { type: String, required: true},
+    name: { type: String, required: true},
+    category: { type: String, required: true},
+    summary: { type: String},
+    images: { type: String, required: true},
+    price: { type: Number, required: true},
+    quantity: { type: Number, required: true},
+    
+});
+
+module.exports = mongoose.model("Post", postSchema);
